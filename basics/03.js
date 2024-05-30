ddbb5dfb871cb247d3e035d44d40f00fb7107c11
Tanishravariya <ravariyatanish@gmail.com>
2024-05-30 15:27:29 +0000

    stack heap

--- basics/03.js
+++ basics/03.js
@@ -1,13 +1,60 @@
 let name1="fl;thn"
-console.log( typeof age);
+// console.log( typeof age);
 
-let value_in_num=Boolean(name1)
-console.log(typeof value_in_num);
-console.log(value_in_num);
-// "33"=33
-// "33abc"=NaN
-// true=1,false=0
-// null=0, undefined=nan
-//44="44"
-//""=false
-//:"kmkldne"=true
+// let value_in_num=Boolean(name1)
+// console.log(typeof value_in_num);
+// // console.log(value_in_num);
+// // "33"=33
+// // "33abc"=NaN
+// // true=1,false=0
+// // null=0, undefined=nan
+// //44="44"
+// //""=false
+// //:"kmkldne"=true
+// let str1=1
+// let str2=str1++
+// console.log(str1)
+// console.log(1 === "3");
+//primitive
+//7 string, num,boolean,null,undefined,symbol,,bigint
+//non-primitive
+//arrays,objects,functions
+const friendArray=["tanish","prince"]
+let mySelf={
+    name:"Tanish",
+    age:89,
+    email:"ravariyatanish@gmail.com",
+}
+console.log(mySelf.email);
+let myself2=mySelf
+myself2.email="hari@gmail.com"
+console.log(myself2.email);
+console.log(mySelf.email);
+
+//     console.log("hello world");
+// }
+// let tan;
+// let juice=null
+// console.log(typeof("5"));
+// console.log(typeof(5));
+// console.log(typeof(5.89));
+// console.log(typeof(958309476098473089n));
+// console.log(typeof(true));
+
+let name="tanish"
+let name2= name
+console.log(name2);
+name2="ravariya"
+console.log(name);
+console.log(name2);
+// stack memory copy of value, heap memory memory reference means actual memory//
+
+
+
+
+
+
+
+
+
+

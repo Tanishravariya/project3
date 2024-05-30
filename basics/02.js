6651eeff228c3c98ae64c6acb4ea9b78d7a655d5
Tanishravariya <ravariyatanish@gmail.com>
2024-05-30 08:49:58 +0000

    container config

--- basics/02.js
+++ basics/02.js
@@ -1,2 +1,7 @@
-console.log("tanishno");
-console.table(typeof undefined);
+
+let tanish="tanish"
+let age=67
+let girlfriend= null
+let you;
+console.log();
+console.table([age,you,girlfriend,tanish])
---
+++ basics/03.js
@@ -0,0 +1,6 @@
+let age=true
+console.log( typeof age);
+
+let value_in_num=String(age)
+console.log(typeof value_in_num);
+console.log(value_in_num);

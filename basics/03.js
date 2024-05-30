bb22cab59467e0a09c6875e03baa1398ba9e5d68
Tanishravariya <ravariyatanish@gmail.com>
2024-05-30 10:48:12 +0000

    conversion

--- basics/03.js
+++ basics/03.js
@@ -1,6 +1,13 @@
-let age=true
+let name1="fl;thn"
 console.log( typeof age);
 
-let value_in_num=String(age)
+let value_in_num=Boolean(name1)
 console.log(typeof value_in_num);
 console.log(value_in_num);
+// "33"=33
+// "33abc"=NaN
+// true=1,false=0
+// null=0, undefined=nan
+//44="44"
+//""=false
+//:"kmkldne"=true
